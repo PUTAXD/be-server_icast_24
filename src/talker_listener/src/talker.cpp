c11951f89a49b6fde552f52833fd84979202cbd9
ramamimu <ahhmaad.blogger@gmail.com>
2023-03-01 00:34:14 +0700

    feat: publish using message generation

--- src/talker_listener/src/talker.cpp
+++ src/talker_listener/src/talker.cpp
@@ -1,5 +1,4 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "talker_listener/Message.h"
 
 #include <sstream>
@@ -8,33 +7,28 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "node_talker");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("topic_chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<talker_listener::Message>("topic_chatter", 1000);
 
   ros::Rate loop_rate(10);
 
   int count = 0;
   while (ros::ok())
   {
-    // std_msgs::String msg2;
-    std_msgs::String msg;
-    // back-end::Message msg;
+    talker_listener::Message msg2;
 
     std::stringstream ss;
-    // std::stringstream ss2;
+    std::stringstream ss2;
     ss << "hello world " << count;
-    msg.data = ss.str();
-    // msg.first_name = ss.str();
-    // ss2 << "i am tested " << count + 2;
-    // msg.last_name = ss2.str();
-    // msg.age = (count * 2) % 200;
-    // msg.score = count * 2;
+    msg2.first_name = ss.str();
+    ss2 << "i am tested " << count + 2;
+    msg2.last_name = ss2.str();
+    msg2.age = (count * 2) % 200;
+    msg2.score = count * 2;
 
+    ROS_INFO("%s", msg2.first_name.c_str());
+    ROS_INFO("%s", msg2.last_name.c_str());
 
-    ROS_INFO("%s", msg.data.c_str());
-    // ROS_INFO("%s", msg.first_name.c_str());
-    // ROS_INFO("%s", msg.first_name.c_str());
-
-    chatter_pub.publish(msg);
+    chatter_pub.publish(msg2);
 
     ros::spinOnce();
 
