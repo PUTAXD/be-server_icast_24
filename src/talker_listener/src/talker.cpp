dd76b9d3b6b82ee6534609e7506b4268be1f9374
ramamimu <ahhmaad.blogger@gmail.com>
2023-03-01 00:24:50 +0700

    feat: success using Message.msg

--- src/talker_listener/src/talker.cpp
+++ src/talker_listener/src/talker.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "talker_listener/Message.h"
 
 #include <sstream>
 
@@ -14,13 +15,24 @@ int main(int argc, char **argv)
   int count = 0;
   while (ros::ok())
   {
+    // std_msgs::String msg2;
     std_msgs::String msg;
+    // back-end::Message msg;
 
     std::stringstream ss;
+    // std::stringstream ss2;
     ss << "hello world " << count;
     msg.data = ss.str();
+    // msg.first_name = ss.str();
+    // ss2 << "i am tested " << count + 2;
+    // msg.last_name = ss2.str();
+    // msg.age = (count * 2) % 200;
+    // msg.score = count * 2;
+
 
     ROS_INFO("%s", msg.data.c_str());
+    // ROS_INFO("%s", msg.first_name.c_str());
+    // ROS_INFO("%s", msg.first_name.c_str());
 
     chatter_pub.publish(msg);
 
