4e58d5f90b252cbb713e5afde91839e8469bcf0b
ramamimu <ahhmaad.blogger@gmail.com>
2023-03-08 00:19:04 +0700

    feat: multicast receive and send

--- src/basestation/src/basestation.cpp
+++ src/basestation/src/basestation.cpp
@@ -1,1 +1,45 @@
 #include "ros/ros.h"
+#include "communications/BS2PC.h"
+
+ros::Publisher bs2pc_pub;
+
+ros::Timer timer_cllbck_be2fe;
+
+communications::BS2PC msg;    
+
+void cllbckSndBS2PC(const ros::TimerEvent& event);
+
+int main(int argc, char *argv[]){
+    ros::init(argc, argv, "basestation");
+    ros::NodeHandle n;
+    ros::MultiThreadedSpinner spinner(4);
+
+    bs2pc_pub = n.advertise<communications::BS2PC>("bs2pc", 1000);
+
+    timer_cllbck_be2fe = n.createTimer(ros::Duration(0.01), cllbckSndBS2PC);
+
+    spinner.spin();
+    return 0;
+}
+
+void cllbckSndBS2PC(const ros::TimerEvent& event){
+    msg.header_manual_and_calibration = 0;
+    msg.command = 1;
+    msg.ball_x_in_field = 2;
+    msg.ball_y_in_field = 3;
+    msg.target_manual_x = 4;
+    msg.target_manual_y = 5;
+    msg.target_manual_theta = 6;
+    msg.offset_robot_x = 7;
+    msg.offset_robot_y = 8;
+    msg.offset_robot_theta = 9;
+    msg.mux1 = 12;
+    msg.mux2 = 13;
+    msg.mux_bs_control = 14;
+    msg.control_v_linear = {15, 16, 17, 18, 19};
+    msg.control_v_angular = {20, 21, 22, 23, 24};
+    msg.control_power_kicker = {25, 26, 27, 28, 29};
+    msg.passing_counter = 30;
+
+    bs2pc_pub.publish(msg);
+}
--- src/communications/src/comm_multicast.cpp
+++ src/communications/src/comm_multicast.cpp
@@ -1,28 +1,35 @@
 #include "communications/multicast.h"
 #include "communications/PC2BS.h"
+#include "communications/BS2PC.h"
 
 #define N_ROBOT 5
 #define LEN_MSG 256
 
 communications::PC2BS pc2bs_msg;
 
+ros::Subscriber bs2pc_sub;
 ros::Publisher pc2bs_pub[N_ROBOT];
 
-void cllbckRcv(const ros::TimerEvent& event);
+void cllbckRcvMtcast(const ros::TimerEvent& event);
+void cllbckSndMtcast(const communications::BS2PC::ConstPtr& msg);
 
 int main(int argc, char *argv[]){
     ros::init(argc, argv, "comm_multicast");
     ros::NodeHandle n;
     ros::MultiThreadedSpinner spinner(4);
+
     ros::Timer timer_cllbck_rcv;
+    ros::Timer timer_cllbck_snd;
 
     openSocket();
     
-    timer_cllbck_rcv = n.createTimer(ros::Duration(0.005), cllbckRcv);
+    timer_cllbck_rcv = n.createTimer(ros::Duration(0.001), cllbckRcvMtcast);
+
+    bs2pc_sub = n.subscribe("bs2pc", 1000, cllbckSndMtcast);
 
     for(int i = 0; i < N_ROBOT; i++){
         char str_topic[100];
-        sprintf(str_topic, "pc2bs_r%d_msg", i+1);
+        sprintf(str_topic, "pc2bs_r%d", i+1);
         pc2bs_pub[i] = n.advertise<communications::PC2BS>(str_topic, 1000);
     }
 
@@ -31,7 +38,7 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-void cllbckRcv(const ros::TimerEvent& event){
+void cllbckRcvMtcast(const ros::TimerEvent& event){
     char recv_buf[LEN_MSG];
     uint8_t nrecv = recvfrom(sd, recv_buf, LEN_MSG, MSG_DONTWAIT, &src_addr, &addr_len);
     
@@ -108,4 +115,91 @@ void cllbckRcv(const ros::TimerEvent& event){
 
         pc2bs_pub[n_robot-1].publish(pc2bs_msg);
     }
+}
+
+void cllbckSndMtcast(const communications::BS2PC::ConstPtr& msg){
+    int counter = 0;
+    int data_size = 0;
+
+    char send_buf[LEN_MSG];
+
+    data_size = 4;
+    memcpy(send_buf, "its0", data_size);
+    counter += data_size;
+    
+    data_size = sizeof(int8_t);
+    memcpy(send_buf + counter, &msg->header_manual_and_calibration, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int8_t);
+    memcpy(send_buf + counter, &msg->command, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->ball_x_in_field, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->ball_y_in_field, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->target_manual_x, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->target_manual_y, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->target_manual_theta, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->offset_robot_x, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->offset_robot_y, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->offset_robot_theta, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->mux1, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->mux2, data_size);
+    counter += data_size;
+
+    data_size = sizeof(int16_t);
+    memcpy(send_buf + counter, &msg->mux_bs_control, data_size);
+    counter += data_size;
+
+    for (uint8_t i = 0; i < 5; i++){
+        data_size = sizeof(uint8_t);
+        memcpy(send_buf + counter, &msg->control_v_linear[i], data_size);
+        counter += data_size;
+    }
+
+    for (uint8_t i = 0; i < 5; i++){
+        data_size = sizeof(uint8_t);
+        memcpy(send_buf + counter, &msg->control_v_angular[i], data_size);
+        counter += data_size;
+    }
+
+    for (uint8_t i = 0; i < 5; i++){
+        data_size = sizeof(uint8_t);
+        memcpy(send_buf + counter, &msg->control_power_kicker[i], data_size);
+        counter += data_size;
+    }
+
+    data_size = sizeof(uint8_t);
+    memcpy(send_buf + counter, &msg->passing_counter, data_size);
+    counter += data_size;
+
+    uint sent = sendto(sd, send_buf, counter, 0, (struct sockaddr *) &localSock, sizeof(struct sockaddr));
 }
